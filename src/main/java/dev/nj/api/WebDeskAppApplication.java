de6cf5f1cd36d327adb08df4514789cb63c8a979
eucarizan <eucarizan@gmail.com>
2024-05-07 23:15:11 +0800

    initial commit

---
+++ src/main/java/dev/nj/api/WebDeskAppApplication.java
@@ -0,0 +1,13 @@
+package dev.nj.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDeskAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDeskAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nj/api/WebDeskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nj.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDeskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
